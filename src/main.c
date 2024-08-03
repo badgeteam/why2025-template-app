cb5bb3dbf9e3defac045388fab424d999c972b24
RobotMan2412 <julian@scheffers.net>
2024-08-03 18:47:17 +0200

    Le working app

--- src/main.c
+++ src/main.c
@@ -1,6 +1,13 @@
 
+#include "bsp.h"
+
 #include <stdio.h>
 
+static bsp_event_t event;
+
 void _start() {
-    printf("Hi, Ther.\n");
+    printf("Hello, World!\n");
+    printf("Funny app is waiting for event...\n");
+    bsp_event_wait(&event, UINT64_MAX);
+    printf("There is an event!\n");
 }
